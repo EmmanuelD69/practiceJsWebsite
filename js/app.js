5c82e073225976a47d2ed801ba2486c3b161f654
EmmanuelD69 <emmanueldevfr@gmail.com>
2020-08-20 13:30:20 +0200

    add GSAP animations

--- js/app.js
+++ js/app.js
@@ -9,7 +9,25 @@ function animateSlides() {
 	const nav = document.querySelector('.nav-header');
 	/* Looping over each slide */
 	sliders.forEach((slide) => {
-        const revealImg = slide.querySelector('.reveal-img');
-        const img = 
+		/* Selecting the elements to animate */
+		const revealImg = slide.querySelector('.reveal-img');
+		const img = slide.querySelector('img');
+		const revealText = slide.querySelector('.reveal-text');
+
+		/* GSAP animations */
+		/* Creating a timeline for our animations */
+		const slideTl = gsap.timeline({
+			/* to check ease animations: https://greensock.com/ease-visualizer/ */
+			defaults: { duration: 1, ease: 'sine.inOut' },
+		});
+
+		/* animations */
+		/* timeline.fromTo(element to animate, from, to, duration) */
+		slideTl.fromTo(nav, { y: '-100%' }, { y: '0%' }, '0.5');
+		slideTl.fromTo(revealImg, { x: '0%' }, { x: '100%' }, '0.8');
+		slideTl.fromTo(img, { scale: 2 }, { scale: 1 }, '2');
+		slideTl.fromTo(revealText, { x: '0%' }, { x: '100%' }, '2.3');
 	});
 }
+
+animateSlides();
