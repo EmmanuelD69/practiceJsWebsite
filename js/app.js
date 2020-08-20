db0870740aad41b4e3a5f361b8bae1f010211e60
EmmanuelD69 <emmanueldevfr@gmail.com>
2020-08-20 12:22:53 +0200

    add html comments + Js animateSlide Function part1

--- js/app.js
+++ js/app.js
@@ -4,4 +4,12 @@ let slideScene;
 function animateSlides() {
 	/* Controller Init */
 	controller = new ScrollMagic.Controller();
+	/* Selecting Elements */
+	const sliders = document.querySelectorAll('.slide');
+	const nav = document.querySelector('.nav-header');
+	/* Looping over each slide */
+	sliders.forEach((slide) => {
+        const revealImg = slide.querySelector('.reveal-img');
+        const img = 
+	});
 }
